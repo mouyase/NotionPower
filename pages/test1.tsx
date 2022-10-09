5b3e3cdb8baf4dceb85219c859f99b43f21f11db
某亚瑟 <mouyase@qq.com>
2022-10-10 01:35:42 +0800

    wip: 修改vscode配置

--- pages/test1.tsx
+++ pages/test1.tsx
@@ -1,17 +1,17 @@
-import { getUserData } from '../libs/notion/getUserData'
-import { getDatabaseData } from '../libs/notion/getDatabaseData'
+import { getUserData } from "../libs/notion/getUserData";
+import { getDatabaseData } from "../libs/notion/getDatabaseData";
 
 const Test1 = ({ res }: any) => {
-  return (<>{res}</>)
-}
-export default Test1
+  return <>{res}</>;
+};
+export default Test1;
 
 export async function getStaticProps() {
-  const res = await getDatabaseData()
+  const res = await getDatabaseData();
 
   return {
     props: {
-      res: JSON.stringify(res)
-    }
-  }
+      res: JSON.stringify(res),
+    },
+  };
 }
--- pages/test2.tsx
+++ pages/test2.tsx
@@ -1,18 +1,18 @@
-import { getUserData } from '../libs/notion/getUserData'
-import { getDatabaseData } from '../libs/notion/getDatabaseData'
-import { getPostData } from '../libs/notion/getPostData'
+import { getUserData } from "../libs/notion/getUserData";
+import { getDatabaseData } from "../libs/notion/getDatabaseData";
+import { getPostData } from "../libs/notion/getPostData";
 
 const Test2 = ({ res }: any) => {
-  return (<>{res}</>)
-}
-export default Test2
+  return <>{res}</>;
+};
+export default Test2;
 
 export async function getStaticProps() {
-  const res = await getPostData()
+  const res = await getPostData('');
 
   return {
     props: {
-      res: JSON.stringify(res)
-    }
-  }
+      res: JSON.stringify(res),
+    },
+  };
 }
